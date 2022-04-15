484ea5b0d8ae6defc56e0b21c91673be5756153f
leo <80543539+justleoo@users.noreply.github.com>
2022-04-15 18:10:57 -0300

    first commit

---
+++ theme.lua
@@ -0,0 +1,29 @@
+local bg = '#0f0f17'
+local fg = '#bfc9db'
+
+return {
+    color_background = bg,
+    color_foreground = fg,
+    color_window_background = bg,
+    color_bold = fg,
+    color_cursor = fg,
+    color_cursor_foreground = '#bfc9db',
+    color_highlight = fg,
+    color_highlight_foreground = bg,
+    color_0 = '#13141c',
+    color_1 = '#d78787',
+    color_2 = '#afbea2',
+    color_3 = '#e4c9af',
+    color_4 = '#a1bdce',
+    color_5 = '#d7beda',
+    color_6 = '#b1e7dd',
+    color_7 = '#bfc9db',
+    color_8 = '#646a73',
+    color_9 = '#d78787',
+    color_10 = '#afbea2',
+    color_11 = '#e4c9af',
+    color_12 = '#a1bdce',
+    color_13 = '#d7beda',
+    color_14 = '#b1e7dd',
+    color_15 = '#858893'
+}
